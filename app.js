0a700fff739753f7e216c1b8a542a9c6c570b3ff
Adam <thomasadam83@hotmail.com>
2020-09-17 23:06:16 -0400

    Added HTML and Node.js files

---
+++ app.js
@@ -0,0 +1,43 @@
+var http = require('http');
+var fs = require('fs');
+var index = fs.readFileSync( 'index.html');
+
+var SerialPort = require('serialport');
+const parsers = SerialPort.parsers;
+
+const parser = new parsers.Readline({
+    delimiter: '\r\n'
+});
+
+var port = new SerialPort('/dev/tty.usbmodem1421',{ 
+    baudRate: 9600,
+    dataBits: 8,
+    parity: 'none',
+    stopBits: 1,
+    flowControl: false
+});
+
+port.pipe(parser);
+
+var app = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(index);
+});
+
+var io = require('socket.io').listen(app);
+
+io.on('connection', function(socket) {
+    
+    console.log('Node is listening to port');
+    
+});
+
+parser.on('data', function(data) {
+    
+    console.log('Received data from port: ' + data);
+    
+    io.emit('data', data);
+    
+});
+
+app.listen(3000);
